f8fb5f310e2ac3a7c7d7d728a2ab8b4f6d019bb7
Timothy Yang <timothyy208@gmail.com>
2019-02-04 04:20:00 -0500

    4.2

--- Bip/ViewController.swift
+++ Bip/ViewController.swift
@@ -30,8 +30,10 @@ class ViewController: UIViewController {
     }
     
     
-    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-    }
+
     
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+        animateImage()
+    }
 }
 
