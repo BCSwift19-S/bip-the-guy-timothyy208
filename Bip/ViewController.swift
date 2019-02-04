cbcf68ebce203151667ef05cdc7253cf26878871
Timothy Yang <timothyy208@gmail.com>
2019-02-04 04:13:36 -0500

    4.1

--- Bip/ViewController.swift
+++ Bip/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageToPunch: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-
+    func animateImage() {
+        let bounds = self.imageToPunch.bounds
+        
+        self.imageToPunch.bounds = CGRect(x: self.imageToPunch.bounds.origin.x + 60, y: self.imageToPunch.bounds.origin.y + 60, width: self.imageToPunch.bounds.width - 60, height: self.imageToPunch.bounds.height - 60)
+        UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: {self.imageToPunch.bounds = bounds}, completion: nil)
+    }
+    @IBAction func openPhotoLibrary(_ sender: UIButton) {
+    }
+    
+    @IBAction func takePhoto(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
+    }
+    
 }
 
